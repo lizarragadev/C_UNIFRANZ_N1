964c8e4dcb71d79873adf98a033d2b859521eead
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-06-02 14:50:38 -0400

    Ejemplo basico de animaciones

---
+++ P6_Animations/app/src/main/java/com/miramicodigo/p6_animations/SplashscreenActivity.java
@@ -0,0 +1,19 @@
+package com.miramicodigo.p6_animations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    /**
+     * @author Gustavo Lizarraga
+     * @date 02-06-2017
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+    }
+}
