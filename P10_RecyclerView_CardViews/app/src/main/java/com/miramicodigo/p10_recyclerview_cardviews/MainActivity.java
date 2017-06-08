2a9144538d7c2da52ef5857d2b65f5e19ebd9c5e
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-06-07 23:18:59 -0400

    Uso de reyclerVie

---
+++ P10_RecyclerView_CardViews/app/src/main/java/com/miramicodigo/p10_recyclerview_cardviews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.miramicodigo.p10_recyclerview_cardviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
