35e4c29d3f5fde9269c0aa1f7ef63e4f6c9dfa4f
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-06-09 18:22:12 -0400

    Uso de Multimedia

---
+++ P11_MediaPlayer/app/src/main/java/com/miramicodigo/p11_mediaplayer/MainActivity.java
@@ -0,0 +1,79 @@
+package com.miramicodigo.p11_mediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * @author Gustavo Lizarraga
+     * @version 1.0
+     * @date 13-04-17
+     *
+     * */
+
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnStop;
+    private ImageView ivAlpha;
+
+    private Animation animacion;
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        ivAlpha = (ImageView) findViewById(R.id.ivAlpha);
+
+        animacion = AnimationUtils.loadAnimation(
+                getApplicationContext(), R.anim.blink);
+
+        initUI();
+        initMediaPlayer();
+    }
+
+    public void initUI() {
+        btnPlay.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnStop.setEnabled(false);
+    }
+
+    public void initMediaPlayer() {
+        mediaPlayer = MediaPlayer.create(
+                getApplicationContext(),
+                R.raw.acdc);
+    }
+
+    public void play(View view) {
+        try {
+
+        } catch (Exception e){
+            System.out.println("Hubo un error: ");
+        }
+    }
+
+    public void pause(View view) {
+        if(mediaPlayer.isPlaying()){
+
+        }
+    }
+
+    public void stop(View view) {
+        if(mediaPlayer.isPlaying()){
+            
+        }
+    }
+}
